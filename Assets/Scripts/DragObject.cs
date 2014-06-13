fd34b7fddb5c9aa54f1a0b1755470411f3a78d15
FlorianBasso <florian@ideanov.com>
2014-06-13 13:01:04 +0200

    generate grid and place pipes randomly

---
+++ Assets/Scripts/DragObject.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DragObject : MonoBehaviour {
+	
+	Vector3 screenPoint;
+	Vector3 offset;
+	Color initialColor;
+
+	// Use this for initialization
+	void Start () {
+		initialColor = this.renderer.material.color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnMouseDown() 
+	{
+		Vector3 scanPos = gameObject.transform.position;
+		screenPoint = Camera.main.WorldToScreenPoint(scanPos);
+		offset = scanPos - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+	}	
+	
+	void OnMouseUp()
+	{
+		this.renderer.material.color = initialColor;
+	}
+	void OnMouseDrag() 
+	{
+		this.renderer.material.color = Color.red;
+		
+		//Update object position
+		Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+		Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+		transform.position = curPosition;
+	}
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+	public GameObject[] pipesArray;
+	public GameObject tileBackground;
+	public int rowsAndColumnsNumber;
+	// Use this for initialization
+	void Start () 
+	{
+		GenerateGrid ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void GenerateGrid()
+	{
+		for (int i = 0; i < rowsAndColumnsNumber; i++) 
+		{
+			for (int j = 0; j < rowsAndColumnsNumber; j++) 
+			{
+				//Background
+				Instantiate(tileBackground, new Vector3(i,j,0), Quaternion.identity);
+
+				//Place pipes
+				Instantiate(pipesArray[Random.Range(0, pipesArray.Length)], new Vector3(i,j,0), Quaternion.identity);
+				//Camera position and orthographic size
+				if(i == rowsAndColumnsNumber/2 && j == rowsAndColumnsNumber/2)
+				{
+					if(rowsAndColumnsNumber % 2 == 0)
+						this.transform.position = new Vector3(i,j-0.5f, -10);
+					else
+						this.transform.position = new Vector3(i,j, -10);
+
+					this.camera.orthographicSize = rowsAndColumnsNumber/2 + 1; 
+				}
+			}
+		}
+	}
+}
