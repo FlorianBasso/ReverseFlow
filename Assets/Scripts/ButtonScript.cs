f6480853698173ea10beab348fb599b95039e962
Deedlith <cindy.vuillaume@live.fr>
2014-07-02 22:52:36 +0200

    MAJ

---
+++ Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonScript : MonoBehaviour {
+
+    public string NextScene = "";
+
+    void OnMouseEnter()
+    {
+        
+        renderer.material.color = Color.gray;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.white;
+    }
+
+    void OnMouseDown()
+    {
+        if(!NextScene.Equals(""))
+            Application.LoadLevel(NextScene);
+        if (this.name.Equals("Quit"))
+        {
+            Application.Quit();
+        }
+    }
+}
